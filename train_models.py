5559c6fc37b9e38523381891588fd56d39eeffa3
MarkdenO <markdenouden9@gmail.com>
2025-05-12 12:56:49 +0200

    changed file extension of models to .joblib

--- train_models.py
+++ train_models.py
@@ -38,6 +38,16 @@ def train_mlp_on_ast(dataframe, label_columns, preprocessing_type):
     Y_pred = model.predict(X_test)
     print("MLP Classification Report:")
     print(classification_report(Y_test, Y_pred, target_names=label_columns, zero_division=0))
+
+    # Calculate accuracy and accuracy as Jaccard subset
+    accuracy_test = accuracy_score(Y_test, Y_pred)
+    print("Exact match accuracy (on TEST set):", accuracy_test)
+
+    correct_predictions_test = np.all(Y_test == Y_pred, axis=1) # Exact match
+    at_least_one_correct_test = np.sum(np.sum((Y_test == 1) & (Y_pred == 1), axis=1) > 0) / len(Y_test)
+    print("At least one correct prediction:", at_least_one_correct_test)
+
+
     return model
 
 def train_svm(dataframe, label_columns, preprocessing_type):
@@ -48,6 +58,15 @@ def train_svm(dataframe, label_columns, preprocessing_type):
     Y_pred = model.predict(X_test)
     print("SVM Classification Report:")
     print(classification_report(Y_test, Y_pred, target_names=label_columns, zero_division=0))
+
+    # Calculate accuracy and accuracy as Jaccard subset
+    accuracy_test = accuracy_score(Y_test, Y_pred)
+    print("Exact match accuracy (on TEST set):", accuracy_test)
+
+    correct_predictions_test = np.all(Y_test == Y_pred, axis=1) # Exact match
+    at_least_one_correct_test = np.sum(np.sum((Y_test == 1) & (Y_pred == 1), axis=1) > 0) / len(Y_test)
+    print("At least one correct prediction:", at_least_one_correct_test)
+
     return model
 
 def train_random_forest(dataframe, label_columns, preprocessing_type):
@@ -58,6 +77,15 @@ def train_random_forest(dataframe, label_columns, preprocessing_type):
     Y_pred = model.predict(X_test)
     print("Random Forest Classification Report:")
     print(classification_report(Y_test, Y_pred, target_names=label_columns, zero_division=0))
+
+    # Calculate accuracy and accuracy as Jaccard subset
+    accuracy_test = accuracy_score(Y_test, Y_pred)
+    print("Exact match accuracy (on TEST set):", accuracy_test)
+
+    correct_predictions_test = np.all(Y_test == Y_pred, axis=1) # Exact match
+    at_least_one_correct_test = np.sum(np.sum((Y_test == 1) & (Y_pred == 1), axis=1) > 0) / len(Y_test)
+    print("At least one correct prediction:", at_least_one_correct_test)
+
     return model
 
 def train_knn(dataframe, label_columns, preprocessing_type):
@@ -68,6 +96,15 @@ def train_knn(dataframe, label_columns, preprocessing_type):
     Y_pred = model.predict(X_test)
     print("K-Nearest Neighbors Classification Report:")
     print(classification_report(Y_test, Y_pred, target_names=label_columns, zero_division=0))
+
+    # Calculate accuracy and accuracy as Jaccard subset
+    accuracy_test = accuracy_score(Y_test, Y_pred)
+    print("Exact match accuracy (on TEST set):", accuracy_test)
+
+    correct_predictions_test = np.all(Y_test == Y_pred, axis=1) # Exact match
+    at_least_one_correct_test = np.sum(np.sum((Y_test == 1) & (Y_pred == 1), axis=1) > 0) / len(Y_test)
+    print("At least one correct prediction:", at_least_one_correct_test)
+
     return model
 
 
@@ -91,32 +128,31 @@ def show_example_predictions(X_test, Y_test, Y_pred, label_columns, n=5):
         print("------")
 
 
-def main():
-    
+def main():    
     # AST
     print("\nAST")
     print("Training MLP on AST")
     df_ast = pd.read_pickle('results/ast.pkl')
-    bigru_model = train_mlp_on_ast(df_ast, LABEL_COLUMNS, 'ast')
+    ast_model = train_mlp_on_ast(df_ast, LABEL_COLUMNS, 'ast')
     
     # Save model
     os.makedirs('models', exist_ok=True)
-    joblib.dump(bigru_model, 'models/bigru_model.pkl')
+    joblib.dump(ast_model, 'models/ast_mlp_model.joblib')
 
     print("\nTraining SVM on AST")
     svm_model = train_svm(df_ast, LABEL_COLUMNS, 'ast')
-    joblib.dump(svm_model, 'models/svm_model.pkl')
+    joblib.dump(svm_model, 'models/ast_svm_model.joblib')
 
     # Ngrams
     print("\nNgrams")
     print("Training Random Forest on Ngrams")
     df_ngrams = pd.read_pickle('results/ngrams.pkl')
     rf_model = train_random_forest(df_ngrams, LABEL_COLUMNS, 'ngrams')
-    joblib.dump(rf_model, 'models/rf_model.pkl')
+    joblib.dump(rf_model, 'models/ngrams_rf_model.joblib')
 
     print("\nTraining KNN on Ngrams")
     knn_model = train_knn(df_ngrams, LABEL_COLUMNS, 'ngrams')
-    joblib.dump(knn_model, 'models/knn_model.pkl')
+    joblib.dump(knn_model, 'models/ngrams_knn_model.joblib')
     print("\nTraining Logistic Regression on Ngrams")
 
 
